39e7c513cebfccc5232520f29c325b43b237028f
Mukesh <mukeshecemgr@gmail.com>
2019-07-07 13:12:36 +0530

    Create data_visualization.py

---
+++ data_visualization.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('DataForAlgo/myTestData.csv', header=None)
